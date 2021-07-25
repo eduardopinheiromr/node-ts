fe7e51f3eca908ac43681a957506fce1e5c2f1af
Eduardo Pinheiro <3dup1n@gmail.com>
2021-07-25 04:44:30 -0300

    sqlite3 and migrations added

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import routes from "./routes";
+import path from "path";
 
 const app = express();
 const port = 3000;
 
 app.use(routes);
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(port, () =>
   console.log("Listening on port http://localhost:" + port)
